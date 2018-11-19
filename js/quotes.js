86aca657e6d5ac07bd792646654f93850da38d12
David Goligorsky <d.goligorsky@gmail.com>
2018-11-18 23:10:12 -0500

    update data source to a web-based api that returns json

--- js/quotes.js
+++ js/quotes.js
@@ -2,27 +2,34 @@ const quoteTag = document.querySelector("h1")
 const authorTag = document.querySelector("p")
 const randomTag = document.querySelector("footer img")
 
-let data = []
+// let data = []
 
-// load in the real data
-fetch("./js/quotes.jason").then(function(response) {
-    return response.json()
-}).then(function(jsonData) {
-    data = jsonData
-})
+// // load in the real data
+// fetch("https://api.superhi.com/api/test/quotes/")
+//     .then(response => response.json())
+//     .then(jsonData => {
+//         data = jsonData
+//         getQuote()
+// })
 
 const getQuote = function() {
-
-    if (data.length > 0) {
-    //get a random quote 
-    const randomNumber = Math.floor(Math.random() * data.length)
-    const randomQuote = data[randomNumber]
+    // if (data.length > 0) {
+    // //get a random quote 
+    // const randomNumber = Math.floor(Math.random() * data.length)
+    // const randomQuote = data[randomNumber]
     
-    // and put it in the site
-    quoteTag.innerHTML = randomQuote.quote
-    authorTag.innerHTML = randomQuote.author
-    }
+    // // and put it in the site
+    // quoteTag.innerHTML = randomQuote.quote
+    // authorTag.innerHTML = randomQuote.author
+    // }
 
+
+    fetch("https://api.superhi.com/api/test/quotes/random")
+        .then(response => response.json())
+        .then(jsonData => {
+            quoteTag.innerHTML = jsonData.quote
+            authorTag.innerHTML = jsonData.author
+        })
 }
 
 // run on page load
