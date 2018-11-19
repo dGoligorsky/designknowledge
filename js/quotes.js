3556c4b866486d1373240f74f8e218ba41db5d6c
David Goligorsky <d.goligorsky@gmail.com>
2018-11-18 23:20:24 -0500

    finish up with some styling

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,6 @@
 const quoteTag = document.querySelector("h1")
 const authorTag = document.querySelector("p")
+const bodyTag = document.querySelector("body")
 const randomTag = document.querySelector("footer img")
 
 // let data = []
@@ -27,8 +28,16 @@ const getQuote = function() {
     fetch("https://api.superhi.com/api/test/quotes/random")
         .then(response => response.json())
         .then(jsonData => {
-            quoteTag.innerHTML = jsonData.quote
-            authorTag.innerHTML = jsonData.author
+            quoteTag.innerHTML = "&ldquo;" + jsonData.quote + "&rdquo;"
+            authorTag.innerHTML = "&mdash; " + jsonData.author
+
+            if (jsonData.quote.length > 100) {
+                quoteTag.classList.add("long")
+            } else {
+                quoteTag.classList.remove("long")
+            }
+
+            bodyTag.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 90%)`
         })
 }
 
